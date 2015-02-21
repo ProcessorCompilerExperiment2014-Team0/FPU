0dcb1d23b9681203b0843a23c29527ff1269970e
Yuito Murase <yuito.murase@gmail.com>
2015-02-21 12:07:26 +0900

    fixing finv.vhd and finv_tb.vhd

--- finv_tb.vhd
+++ finv_tb.vhd
@@ -1,50 +1,50 @@
 -- TestBench Template 
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
-use IEEE.std_logic_textio.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
 
 use std.textio.all;
 
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+library work;
+use work.finv_p.all;
 
 ENTITY finv_tb IS
 END finv_tb;
 
 ARCHITECTURE behavior OF finv_tb IS
 
-  -- Component Declaration
-  COMPONENT finv
-    PORT(clk : in std_logic;
-         a : IN std_logic_vector(31 downto 0);
-         s : OUT std_logic_vector(31 downto 0));
-  END COMPONENT;
-
-  SIGNAL i1 :  std_logic_vector(31 downto 0);
-  SIGNAL o1 :  std_logic_vector(31 downto 0);
+  signal xrst, stall : std_logic;
+  signal i1 :  unsigned(31 downto 0);
+  signal o1 :  unsigned(31 downto 0);
   signal clk : std_logic;
   signal terminate : std_logic := '0';
-  signal t_start : std_logic_vector(31 downto 0) := x"00000000";
-  signal t_end   : std_logic_vector(31 downto 0) := x"00000000";
+  signal t_start : unsigned(31 downto 0) := x"00000000";
+  signal t_end   : unsigned(31 downto 0) := x"00000000";
 
   file infile : text is in "./finv_test/testcase.txt";
   file outfile : text is out "./finv_test/result.txt";
   
 BEGIN
 
+  xrst  <= '1';
+  stall <= '0';
+
   -- Component Instantiation
-  uut: finv PORT MAP(
-    clk => clk,
-    a => i1,
-    s => o1);
+  uut : finv PORT MAP(
+    clk   => clk,
+    xrst  => xrst,
+    stall => stall,
+    a     => i1,
+    s     => o1);
 
   --  Test Bench Statements
   tb : process (clk)
     variable my_line, out_line : LINE;
-    variable a, b : std_logic_vector(31 downto 0);
-    variable t : std_logic_vector(31 downto 0);
+    variable a : std_logic_vector(31 downto 0);
+    variable b : unsigned(31 downto 0);
+    variable t : unsigned(31 downto 0);
   BEGIN
 
     if rising_edge(clk) then
@@ -56,7 +56,7 @@ BEGIN
         readline(infile, my_line);
         read(my_line, a);
 
-        i1 <= a;
+        i1 <= unsigned(a);
       else
         t := t_end;
         if t >= 3 then
@@ -69,7 +69,7 @@ BEGIN
       b := o1;
 
       if t_start >= 4 then
-        write(out_line, b);
+        write(out_line, std_logic_vector(b));
         writeline(outfile, out_line);
       end if;
       t_start <= t_start + 1;
